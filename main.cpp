c47666d33ab48f5d68ce6d7ac313c00cd6103027
Andrey Loginov <andreyloginovmob@gmail.com>
2023-01-21 21:08:41 -0500

    Add simple test code and implementation of naive median computation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <random>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
+template <typename T>
+T naiveMedian(T a[], int n) {
+    sort(a, a + n);
+    if (n % 2 == 0) {
+        return a[n / 2 - 1];
+    } else {
+        return a[n / 2];
+    }
+}
+
 int main()
 {
-    cout << "Initial commit!" << endl;
+
+    int n = 20;
+    vector<float> a(n);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<float> dis(0, 100);
+
+    for (int i = 0; i < n; i++) {
+        a[i] = dis(gen); // generates a random number between 1 and 100
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    cout << "Median:"<<naiveMedian<float>(a.data(),a.size());
     return 0;
 }
